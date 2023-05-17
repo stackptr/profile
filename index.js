c8aa27554b3838618a637168eebdbb21b54defbf
✿ corey (they/them) <corey@x64.co>
2023-05-16 18:34:40 -0700

    Formatting

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 var express = require('express'),
-    path = require('path');
+  path = require('path')
 
-var src = path.join(__dirname, '/public');
+var src = path.join(__dirname, '/public')
 
-var app = express();
+var app = express()
 
-app.use(express.static(src));
+app.use(express.static(src))
 
-app.use('/.well-known', express.static('well-known'));
+app.use('/.well-known', express.static('well-known'))
 
 app.get('/', function (req, res) {
-  res.sendFile(src + '/index.html');
-});
+  res.sendFile(src + '/index.html')
+})
 
-app.get('/.well-known/webfinger', function(req, res) {
+app.get('/.well-known/webfinger', function (req, res) {
   if (req.query && req.query.resource === 'acct:corey@x64.co') {
-    res.sendFile(src + '/identity.json');
+    res.sendFile(src + '/identity.json')
   }
-});
+})
 
-app.listen(3001, function (){
-  console.log('Server started');
-});
+app.listen(3001, function () {
+  console.log('Server started')
+})
--- public/main.js
+++ public/main.js
@@ -1,62 +1,58 @@
 $(function () {
   // Set gravatar/email
-  var email = "corey@x64.co",
-    hash = md5(email.trim().toLowerCase());
+  var email = 'corey@x64.co',
+    hash = md5(email.trim().toLowerCase())
 
-  $(".email").attr("href", "mailto:" + email);
-  $(".avatar").css(
-    "background-image",
-    "url(https://www.gravatar.com/avatar/" + hash + "?s=300)"
-  );
+  $('.email').attr('href', 'mailto:' + email)
+  $('.avatar').css('background-image', 'url(https://www.gravatar.com/avatar/' + hash + '?s=300)')
 
-  var events =
-    "webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend";
+  var events = 'webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend'
 
   var animCallback = function (el, cb) {
     el.on(events, function (e) {
       if (e.target === e.currentTarget) {
-        cb();
-        el.off(events);
+        cb()
+        el.off(events)
       }
-    });
-  };
+    })
+  }
 
-  var $avatar = $(".avatar"),
-    $header = $(".title"),
-    $resume = $(".resume"),
-    $body = $("article"),
-    $social = $(".social > li");
+  var $avatar = $('.avatar'),
+    $header = $('.title'),
+    $resume = $('.resume'),
+    $body = $('article'),
+    $social = $('.social > li')
 
   var animSocial = function () {
-    var animQueue = [];
+    var animQueue = []
     $social.each(function () {
-      var el = $(this);
+      var el = $(this)
       animQueue.push(function () {
-        el.removeClass("initial");
-      });
-    });
+        el.removeClass('initial')
+      })
+    })
 
     animQueue.push(function () {
-      $resume.removeClass("initial");
-    });
+      $resume.removeClass('initial')
+    })
 
-    var i = 0;
+    var i = 0
     var intervalId = window.setInterval(function () {
-      animQueue[i++]();
+      animQueue[i++]()
       if (i === animQueue.length) {
-        window.clearInterval(intervalId);
+        window.clearInterval(intervalId)
       }
-    }, 100);
-  };
+    }, 100)
+  }
 
   window.setTimeout(function () {
     animCallback($avatar, function () {
       animCallback($header, function () {
-        $body.removeClass("initial");
-      });
-      $header.removeClass("initial");
-      animSocial();
-    });
-    $avatar.removeClass("initial");
-  }, 300);
-});
+        $body.removeClass('initial')
+      })
+      $header.removeClass('initial')
+      animSocial()
+    })
+    $avatar.removeClass('initial')
+  }, 300)
+})
