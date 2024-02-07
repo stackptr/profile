b144ec3f34274d10ee808739e01de187335cf6d9
✿ corey <corey@x64.co>
2024-02-06 16:46:38 -0800

    Nix flake

---
+++ flake.nix
@@ -0,0 +1,71 @@
+{
+  description = "x64.co";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+        makeWrapper = pkgs.makeWrapper;
+        nodejs = pkgs.nodejs_18;
+        yarn = pkgs.yarn;
+        prefetch-yarn-deps = pkgs.prefetch-yarn-deps;
+        app = pkgs.stdenv.mkDerivation (finalAttrs: {
+          name = "x64.co";
+          src = ./.;
+          offlineCache = pkgs.fetchYarnDeps {
+            yarnLock = "${finalAttrs.src}/yarn.lock";
+            hash = "sha256-01UPIijrdT7wE2Dem3HOyP6DJsVmmiBf6uqGk4PvUp0=";
+          };
+          nativeBuildInputs = [
+            makeWrapper
+            nodejs
+            prefetch-yarn-deps
+            yarn
+          ];
+          configurePhase = ''
+            runHook preConfigure
+
+            export HOME=$(mktemp -d)
+            yarn config --offline set yarn-offline-mirror $offlineCache
+            fixup-yarn-lock yarn.lock
+            yarn --offline --frozen-lockfile --ignore-platform --ignore-scripts --no-progress --non-interactive install
+            patchShebangs node_modules
+
+            runHook postConfigure
+          '';
+
+          buildPhase = ''
+            runHook preBuild
+
+            yarn --offline build
+
+            runHook postBuild
+          '';
+
+          installPhase = ''
+            runHook preInstall
+
+            mkdir -p $out/lib
+            cp -R . $out/lib
+            makeWrapper "${nodejs}/bin/node" "$out/bin/x64.co" \
+              --add-flags "$out/lib/dist/index.js"
+
+            runHook postInstall
+          '';
+        });
+      in {
+        packages = {
+          default = app;
+        };
+      }
+    );
+}
